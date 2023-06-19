adc4974df845cb819a6298334a4769a11d859351
Tony <tony@DESKTOP-C5TKN10.localdomain>
2023-06-19 20:34:18 +1000

    Card Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-cards';
+  posts = [
+  {
+    title: "Tree",
+    userName: "@Tree",
+    imgURL:"assets/tree.jpeg",
+    content: "I saw a free"
+  },
+  {
+    title: "Snowy Mountain",
+    userName: "@Mountainlover",
+    imgURL:"assets/mountain.jpeg",
+    content: "Here is a picture of a mountain"
+  },
+  {
+    title: "Bike",
+    userName: "@Mountainbiking!",
+    imgURL:"assets/biking.jpeg",
+    content: "I did some biking today"
+  },
+  ]
+
 }
